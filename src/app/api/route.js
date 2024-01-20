b0b8be7eff3bf995b7c927580118414682859b30
Jakub Hampl <entitak@gmail.com>
2024-01-20 21:13:12 +0100

    new method for getting jackpots

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -9,6 +9,19 @@ const createJackpot = async (data) => {
   return await Jackpot(data).save();
 };
 
+let lastJackpot = null
+
+const getIndexOfJackpot = (jackpots, lastJackpot) => {
+  return jackpots.findIndex((obj) => (
+    obj.jackpot === lastJackpot.jackpot &&
+    obj.sql_bar === lastJackpot.sql_bar &&
+    obj.sql_city === lastJackpot.sql_city &&
+    obj.sql_inserted === lastJackpot.sql_inserted &&
+    obj.sql_jp_name === lastJackpot.sql_jp_name &&
+    obj.sql_machine === lastJackpot.sql_machine
+  )) || 0
+}
+
 export async function POST(req) {
   const { query } = await req.json()
   const casinoUrl = 'https://casinovictory.leris.cz/modules/playgames_jackpot/script/jackpotservice.php' + query
@@ -18,11 +31,21 @@ export async function POST(req) {
     url: casinoUrl,
   })
 
-  if (data?.data?.jackpot?.length > 0) {
-    console.log('added jackpot', data?.data.jackpot)
-    await createJackpot({
-      jackpot: JSON.stringify(data?.data.jackpot)
-    });
+  const jackpots = data?.data?.history
+  if (lastJackpot !== null) {
+    const indexOfLastJackpot = getIndexOfJackpot(jackpots, lastJackpot)
+    console.log(jackpots, lastJackpot, indexOfLastJackpot)
+    if (indexOfLastJackpot > 0) {
+      for (let i = 0; i < indexOfLastJackpot; i++) {
+        console.log('added jackpot', data.data.history, data.data.history[i])
+        await createJackpot({
+          jackpot: JSON.stringify(data.data.history[i])
+        });
+      }
+      lastJackpot = jackpots[0]
+    }
+  } else {
+    lastJackpot = jackpots[0]
   }
 
   return NextResponse.json(data?.data)
